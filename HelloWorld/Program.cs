321cad5cd0b8db33a3474ae99664f0a75eb86b33
astyer22 <astyer@byui.edu>
2024-06-15 22:19:51 -0600

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Hello
+    {
+        private string _name;
+        private DateTime _date;
+
+        public Hello(string name)
+        {
+            _name = name;
+            _date = DateTime.Now;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello World, this is {_name}! Today is {_date:MMMM dd, yyyy}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Hello hello = new Hello("Austin Styer");
+            hello.SayHello();
+        }
+    }
+}
